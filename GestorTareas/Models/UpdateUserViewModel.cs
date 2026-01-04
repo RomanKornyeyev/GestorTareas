14a62a7e0e61b2c31e87731e894ba4c7a32d9610
roman <romankr1997@gmail.com>
2026-01-04 16:30:18 +0100

    editar usuario

---
+++ GestorTareas/Models/UpdateUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorTareas.Models
+{
+    public class UpdateUserViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(250)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(250)]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [EmailAddress(ErrorMessage = "Se debe introducir un email válido")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
